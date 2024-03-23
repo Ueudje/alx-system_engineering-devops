82ed23859702e5f180828e8cd48c12d13f84b89e
Ueudje <classlol72@gmail.com>
2024-03-22 19:37:40 -0700

    done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-# Create a file using puppet
+# Docs 
 
 file { '/tmp/school':
-	ensure  => file,
-	path    => '/tmp/school',
-	mode    => '0744',
-	owner   => 'www.data',
-	group   => 'www.data',
-	content => 'I love puppet',
-} 
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
